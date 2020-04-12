ee3b976733a275c9814474c98bf6bd745309e7f2
unknown <ayushnabar@outlook.com>
2020-04-11 20:57:08 -0700

    new changes

--- script.js
+++ script.js
@@ -5,10 +5,35 @@ const classNames = {
   TODO_DELETE: 'todo-delete',
 }
 
+// {/* <li>
+//     <input type = "checkbox" />
+//     <button> delete</button>
+//     <span>text</span>
+// </li> */}
+
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-function newTodo() {
-  alert('New TODO button clicked!')
+let todos = []
+
+function renderTodo(todo){
+  //render a single todo
+}
+
+function render(){
+  //render todos in memopry to the page
+  list.innerHTML = ''
+  todos.map(renderTodo).forEach(todo => list.appendChild(todo))
+  return false;
+}
+function addTodo(name){
+  const todo = new todo(name)
+  todos.push(todo)
+  return render()
+}
+function removeTodo(){
+  const todo = this.todoRef
+  todos = todos.filter(t => t !== todo)
+  return render()
 }
